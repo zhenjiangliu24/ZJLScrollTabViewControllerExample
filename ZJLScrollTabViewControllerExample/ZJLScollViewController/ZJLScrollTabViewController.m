f93b40bbbab621e2d807642c2715732442fe57ee
lovehuahua24 <lovehuahua24@gmail.com>
2016-06-26 19:59:38 -0400

    bug fixed

--- ZJLScrollTabViewControllerExample/ZJLScollViewController/ZJLScrollTabViewController.m
+++ ZJLScrollTabViewControllerExample/ZJLScollViewController/ZJLScrollTabViewController.m
@@ -21,6 +21,7 @@ static const CGFloat TopLabelHeight = 40.0;
 @property (nonatomic, strong) UIScrollView *topTabScrollView;
 @property (nonatomic, strong) UIScrollView *contentScrollView;
 @property (nonatomic, strong) UIView *highlightBar;
+@property (nonatomic, assign) NSInteger currentTab;
 @end
 
 @implementation ZJLScrollTabViewController
@@ -30,12 +31,12 @@ static const CGFloat TopLabelHeight = 40.0;
     self.navigationController.navigationBar.translucent = NO;
     self.title = @"ZJLScrollTabView";
     self.view.backgroundColor = [UIColor whiteColor];
-    
+    _currentTab = 0;
     [self initSubScrollViews];
     [self initTopBarLabels];
     [self addContentViewControllers];
     [self scrollViewDidEndScrollingAnimation:_contentScrollView];
-    
+    [self scrollViewDidScroll:_contentScrollView];
 }
 
 - (void)initSubScrollViews
@@ -90,26 +91,38 @@ static const CGFloat TopLabelHeight = 40.0;
 - (void)topLabelClicked:(UITapGestureRecognizer *)tap
 {
     NSInteger index = [tap.view.superview.subviews indexOfObject:tap.view];
+    _currentTab = index;
     CGPoint offset = self.contentScrollView.contentOffset;
     offset.x = index*ZJLScreenWidth;
     [_contentScrollView setContentOffset:offset animated:YES];
+    [self scrollViewDidEndScrollingAnimation:_contentScrollView];
 }
+
+#pragma mark - update top tab scroll view
+//- (void)updateTopScrollView
+//{
+//    NSInteger index = _contentScrollView.contentOffset.x/_contentScrollView.frame.size.width;
+//    CGFloat centerX = TopLabelWidth/2+index*TopLabelWidth;
+//    CGPoint center = CGPointMake(centerX, _highlightBar.center.y);
+//    _highlightBar.center = center;
+//}
 
 #pragma mark - content scroll view scroll action delegate
 
 - (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
 {
-    [self scrollViewDidEndScrollingAnimation:_contentScrollView];
+    [self scrollViewDidEndScrollingAnimation:scrollView];
 }
 
 - (void)scrollViewDidEndScrollingAnimation:(UIScrollView *)scrollView
 {
     NSInteger index = scrollView.contentOffset.x/scrollView.frame.size.width;
+    _currentTab = index;
     UIViewController *testVC = self.childViewControllers[index];
     
     CGPoint offsetPoint = _topTabScrollView.contentOffset;
     UILabel *selectedLabel = _topTabScrollView.subviews[index];
-    offsetPoint.x = selectedLabel.center.x - scrollView.frame.size.width/2;
+    offsetPoint.x = selectedLabel.center.x -scrollView.frame.size.width*0.5;;
     if (offsetPoint.x<0) {
         offsetPoint.x = 0;
     }
@@ -117,11 +130,14 @@ static const CGFloat TopLabelHeight = 40.0;
     if (offsetPoint.x>maxX) {
         offsetPoint.x = maxX;
     }
+    [_topTabScrollView setContentOffset:offsetPoint animated:YES];
     if ([testVC isViewLoaded]) {
         return;
     }
+    
     testVC.view.frame = CGRectMake(scrollView.contentOffset.x, 0, scrollView.frame.size.width, scrollView.frame.size.height);
     [_contentScrollView addSubview:testVC.view];
+    //[self updateTopScrollView];
 }
 
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView
@@ -129,6 +145,9 @@ static const CGFloat TopLabelHeight = 40.0;
     CGFloat num = scrollView.contentOffset.x/scrollView.frame.size.width;
     NSLog(@"content offset %f width is %f",scrollView.contentOffset.x,scrollView.frame.size.width);
     NSInteger index = num;
+    CGFloat centerX = TopLabelWidth/2+index*TopLabelWidth;
+    CGPoint center = CGPointMake(centerX, _highlightBar.center.y);
+    _highlightBar.center = center;
     if (index == _numberOfTabs-1) {
         UILabel *label = _topTabScrollView.subviews[index];
         label.ZJLScale = 1 - (num - index);
@@ -139,9 +158,7 @@ static const CGFloat TopLabelHeight = 40.0;
     currentLabel.ZJLScale = 1 - (num - index);
     nextLabel.ZJLScale = num - index;
     
-    CGFloat centerX = TopLabelWidth/2+num*TopLabelWidth;
-    CGPoint center = CGPointMake(centerX, _highlightBar.center.y);
-    _highlightBar.center = center;
+    
 }
 
 @end
